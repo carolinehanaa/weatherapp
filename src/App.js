6c65872657b093df79d9249c896a20e559ba67a3
carolinehanaa <carolinehana28@gmail.com>
2023-03-24 15:35:56 -0700

    peer rev

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+// Peer review by Madeline G.: I love the choice of colors on this project! The simplicity and hard edges are really pleasing to the eye. The symbols used for the different weather conditions match well too. There is no functioning favorites section, but of the code that is there, everything seems to work as it should. Good job!!
+
+
 import './App.css';
 import Current from './components/Current.js'
 
